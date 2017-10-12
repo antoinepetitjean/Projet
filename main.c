aa2d1529ddede397b94b87f35ff7f4e2dfd1d95a
antoinepetitjean <31929970+antoinepetitjean@users.noreply.github.com>
2017-10-12 15:14:18 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,76 @@
 
 #define SCREEN_WIDTH  640
 #define SCREEN_HEIGHT 640
+#define LIGNE_FOND 15
+#define COLONE_FOND 15
+
+struct t_carre *fond[LIGNE_FOND][COLONE_FOND];
+
+void lire_fichier(char * f)
+{
+    FILE* fichier = NULL;
+    int i,j,ligne;
+    char c;
+    struct t_carre * case_bloque;//erreur ici
+    case_bloque->deplacable = 0;
+
+    fichier = fopen(f, "r");
+
+    if (fichier != NULL)
+
+    {
+      ligne=0;
+        while (fgetc(fichier) != '\n' && ligne<LIGNE_FOND)
+	{
+	  fseek(fichier, -1, SEEK_CUR);
+	  for (i=0; i<COLONE_FOND; i++)
+	  {
+	    c=fgetc(fichier);
+	    
+	    if(c != '#')
+	    {
+	       //printf("#");
+	      fond[ligne][i]=case_bloque;
+	    }
+	    
+	    else if (c=='\n')
+	    {
+	      for(j=i;j<COLONE_FOND;j++)
+	      {
+		fond[ligne][j]=case_bloque;
+	      }
+	      break;
+	    }
+	    
+	   /* else
+	    {
+	       //printf(" ");
+	      fond[ligne][i]=NULL;
+	    }*/
+	    
+	  }
+	  ligne++;
+	  //printf("\n");
+
+	}
+
+        fclose(fichier);
+	free(case_bloque);
+    }
+}
+
+///***************pour tester le remplissage du tableau fond********************************///
+void affiche_fond()
+{
+  int i,j;
+  for (i=0;i<LIGNE_FOND;i++){
+    for (j=0;j<COLONE_FOND;j++){
+      if(fond[i][j]!=NULL) printf("1");
+    }
+    printf("\n");
+  }
+}
+///****************************************************************************************///
 
 int main(int argc, char *argv[])
 
@@ -12,6 +82,8 @@ int main(int argc, char *argv[])
   SDL_Surface *screen, *temp, *pentaminosp;
   t_carre pentomino;
   SDL_Init(SDL_INIT_VIDEO);
+  
+  //t_piece * liste_des_pieces = malloc(sizeof(t_piece));
 
   SDL_WM_SetCaption("Pentamino", "LUL");
 
@@ -30,6 +102,7 @@ int main(int argc, char *argv[])
 
   int gameover = 0;
   char key[SDLK_LAST] = {0};
+  lire_fichier("pentomino.txt");
 
   while (!gameover)
     {
@@ -38,10 +111,9 @@ int main(int argc, char *argv[])
       SDL_UpdateRect(screen, 0, 0, 0, 0);
       SDL_ShowCursor(1);
     }
-
+  //free(&liste_des_pieces);
   SDL_FreeSurface(pentaminosp);
   SDL_Quit();
-
 
   return 0;
     
--- pentomino.h
+++ pentomino.h
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <SDL.h>
 
+typedef struct point2d{
+  int x;
+  int y;
+}point2d;
+
 typedef struct carre{
-  SDL_Rect pos;
+  SDL_Rect pos;//UTILISATION DU TYPE POINT2D ???
   SDL_Rect sprite;
   int deplacable;
 }t_carre;
@@ -11,6 +16,8 @@ typedef struct carre{
 typedef struct piece{
   t_carre ** p;
 }t_piece;
+
+
 
 extern void rdroite(t_piece * p);
 extern void rgauche(t_piece * p);
